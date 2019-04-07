8dfb8f9f6a1798a3edb9759228632d47e508b28c
Kevin Jonathan <jonathanhallykevin@gmail.com>
2019-04-07 20:28:48 +0400

    Material Dashboard et DB.js

---
+++ src/config/db.js
@@ -0,0 +1,15 @@
+// db.js
+
+import Firebase from 'firebase'
+let config = {
+    apiKey: "AIzaSyAUYC2tw__jqTEG3b2tKfnKPlaDdjWuJyw",
+    authDomain: "itumbds2019restaurant-f74cd.firebaseapp.com",
+    databaseURL: "https://itumbds2019restaurant-f74cd.firebaseio.com",
+    projectId: "itumbds2019restaurant-f74cd",
+    storageBucket: "itumbds2019restaurant-f74cd.appspot.com",
+    messagingSenderId: "410700678330"
+    };
+let app = Firebase.initializeApp(config)
+export const db = app.database()
+export const storage = app.storage()
+export const database = app.firestore();
--- src/material-dashboard.js
+++ src/material-dashboard.js
@@ -7,7 +7,7 @@ import "vue-material/dist/vue-material.min.css";
 import "./assets/scss/material-dashboard.scss";
 
 // library auto imports
-import "es6-promise/auto";
+//import "es6-promise/auto";
 
 /**
  * This is the main Light Bootstrap Dashboard Vue plugin where dashboard related plugins are registerd.
