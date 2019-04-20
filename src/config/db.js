5c031038470ca63af8c2d26d07d31ed0796b1365
kronos <andomanaitu@gmail.com>
2019-04-20 22:21:07 +0400

    test db

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,22 @@
 // db.js
 
 import Firebase from 'firebase'
+// let config = {
+//     apiKey: "AIzaSyAUYC2tw__jqTEG3b2tKfnKPlaDdjWuJyw",
+//     authDomain: "itumbds2019restaurant-f74cd.firebaseapp.com",
+//     databaseURL: "https://itumbds2019restaurant-f74cd.firebaseio.com",
+//     projectId: "itumbds2019restaurant-f74cd",
+//     storageBucket: "itumbds2019restaurant-f74cd.appspot.com",
+//     messagingSenderId: "410700678330"
+//     };
 let config = {
-    apiKey: "AIzaSyAUYC2tw__jqTEG3b2tKfnKPlaDdjWuJyw",
-    authDomain: "itumbds2019restaurant-f74cd.firebaseapp.com",
-    databaseURL: "https://itumbds2019restaurant-f74cd.firebaseio.com",
-    projectId: "itumbds2019restaurant-f74cd",
-    storageBucket: "itumbds2019restaurant-f74cd.appspot.com",
-    messagingSenderId: "410700678330"
-    };
+    apiKey: "AIzaSyCuZ6dtA279CvbUXTSzzPAMPedS5H2oOdk",
+    authDomain: "itumbdsrestaurant.firebaseapp.com",
+    databaseURL: "https://itumbdsrestaurant.firebaseio.com",
+    projectId: "itumbdsrestaurant",
+    storageBucket: "itumbdsrestaurant.appspot.com",
+    messagingSenderId: "436765458051"
+  };
 let app = Firebase.initializeApp(config)
 export const db = app.database()
 export const storage = app.storage()
