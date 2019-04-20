5511629ab113a4826384d2f4e8031b8d001b0943
Kevin Jonathan <jonathanhallykevin@gmail.com>
2019-04-21 00:34:37 +0400

    rectif database config

--- src/config/db.js
+++ src/config/db.js
@@ -9,29 +9,8 @@ let config = {
     projectId: "itumbds2019restaurant-f74cd",
     storageBucket: "itumbds2019restaurant-f74cd.appspot.com",
     messagingSenderId: "410700678330"
-<<<<<<< HEAD
-};
-
-/*let config = {
-    apiKey: "AIzaSyCv49JZpHYxIjla0rg_WSFswN1-PCKYO_0",
-    authDomain: "mbds-dc76d.firebaseapp.com",
-    databaseURL: "https://mbds-dc76d.firebaseio.com",
-    projectId: "mbds-dc76d",
-    storageBucket: "mbds-dc76d.appspot.com",
-    messagingSenderId: "974040669611"
-  };*/
-
-=======
-    };
-// let config = {
-//     apiKey: "AIzaSyCuZ6dtA279CvbUXTSzzPAMPedS5H2oOdk",
-//     authDomain: "itumbdsrestaurant.firebaseapp.com",
-//     databaseURL: "https://itumbdsrestaurant.firebaseio.com",
-//     projectId: "itumbdsrestaurant",
-//     storageBucket: "itumbdsrestaurant.appspot.com",
-//     messagingSenderId: "436765458051"
-//   };
->>>>>>> master
+  };
+  
 let app = Firebase.initializeApp(config)
 export const db = app.database()
 export const storage = app.storage()
