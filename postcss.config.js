f305aa2b8433854811db7adf821d81894a262bbf
kronos <andomanaitu@gmail.com>
2019-04-21 00:44:00 +0400

    devserv

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   plugins: {
     autoprefixer: {}
-  },
-  devServer: {
-    disableHostCheck: true,
-    host: '0.0.0.0'
   }
+  // ,
+  // devServer: {
+  //   disableHostCheck: true,
+  //   host: '0.0.0.0'
+  // }
 }
